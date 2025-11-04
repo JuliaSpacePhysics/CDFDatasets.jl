50ee5fc6f06fe89d0ad207bcacdce2c33db93530
Beforerr <zzj956959688@gmail.com>
2025-11-03 23:35:20 -0800

    feat: implement custom Dataset display with CDF variable categorization

--- src/dataset.jl
+++ src/dataset.jl
@@ -35,7 +35,60 @@ Base.parent(ds::CDFDataset) = ds.source
 Base.getindex(ds::AbstractCDFDataset, name::String) = CDM.variable(ds, name)
 
 function Base.show(io::IO, ::MIME"text/plain", ds::AbstractCDFDataset)
-    return invoke(show, Tuple{IO, AbstractDataset}, io, ds)
+    # return invoke(show, Tuple{IO, AbstractDataset}, io, ds)
+    return _show(io, ds)
+end
+
+function _show(io::IO, ds::AbstractCDFDataset)
+    level = get(io, :level, 0)
+    indent = " "^level
+
+    if !isopen(ds)
+        print(io, "closed Dataset")
+        return
+    end
+
+    printstyled(io, indent, "Dataset: ", CDM.path(ds), "\n", color = CDM.section_color[])
+
+    print(io, indent, "Group: ", CDM.name(ds), "\n")
+    print(io, "\n")
+
+    # show dimensions
+    if length(dimnames(ds)) > 0
+        show_dim(io, dims(ds))
+        print(io, "\n")
+    end
+
+    varnames = keys(ds)
+
+    if length(varnames) > 0
+        printstyled(io, indent, "Data variables\n", color = CDM.section_color[])
+
+        vars = [ds[name] for name in varnames]
+        for var in filter(var -> var_type(var) == "data", vars)
+            show(IOContext(io, :level => level + 2), var)
+            print(io, "\n")
+        end
+
+        support_var_names = filter(var -> var_type(ds[var]) == "support_data", varnames)
+        if length(support_var_names) > 0
+            printstyled(io, indent, "Support variables: "; color = CDM.section_color[])
+            printstyled(io, join(support_var_names, ", "), "\n"; bold = true)
+        end
+
+        meta_var_names = filter(var -> var_type(ds[var]) == "metadata", varnames)
+        if length(meta_var_names) > 0
+            printstyled(io, indent, "Metadata variables: "; color = CDM.section_color[])
+            printstyled(io, join(meta_var_names, ", "), "\n"; bold = true)
+        end
+    end
+
+    # global attribues
+    if length(CDM.attribnames(ds)) > 0
+        printstyled(io, indent, "Global attributes\n", color = CDM.section_color[])
+        CDM.show_attrib(IOContext(io, :level => level + 2), CDM.attribs(ds))
+    end
+    return
 end
 
 function Base.show(io::IO, ds::AbstractCDFDataset)
@@ -65,6 +118,7 @@ end
 CDM.varnames(ds::CDFDataset) = CDM.varnames(ds.source)
 CDM.attribnames(ds::CDFDataset) = CDM.attribnames(ds.source)
 CDM.attrib(ds::CDFDataset, name::String) = CDM.attrib(ds.source, name)
+CDM.path(ds::CDFDataset) = CDM.path(ds.source)
 
 function CDM.name(ds::AbstractCDFDataset)
     return only(get(ds.attrib, "Logical_source", "/"))
@@ -80,6 +134,7 @@ _parent1(ds::ConcatCDFDataset) = ds.sources[1]
 CDM.varnames(ds::ConcatCDFDataset) = CDM.varnames(_parent1(ds))
 CDM.attribnames(ds::ConcatCDFDataset) = CDM.attribnames(_parent1(ds))
 CDM.attrib(ds::ConcatCDFDataset, name::String) = CDM.attrib(_parent1(ds), name)
+CDM.path(ds::ConcatCDFDataset) = CDM.path.(ds.sources)
 
 function CDM.variable(ds::ConcatCDFDataset, name::Union{String, Symbol})
     var1 = _parent1(ds)[name]
