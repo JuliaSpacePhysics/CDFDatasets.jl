b425c21e7ebbbc753a184e4975a3532879afa71f
Beforerr <zzj956959688@gmail.com>
2025-11-18 09:52:33 -0800

    feat: add JET.jl static analysis testing and improve type stability
    - Made CDFVariable name field type-parameterized to improve type stability

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,6 @@
 
 ## [TODO]
 
+- [x] Static analysis test with `JET.jl` 
+- [ ] Full support for `CommonDataModel.jl` interface
 - [ ] Use `ConcatCDFDataset` for `ConcatCDFVariable` so `CDM.dataset` returns the parent dataset
--- src/subvariable.jl
+++ src/subvariable.jl
@@ -5,11 +5,11 @@ function CDM.dim(var::SubCDFVariable, i::Int)
     return @views CDM.dim(parent(var), i)[indices]
 end
 
-function find_indices(var, t0, t1)
-    tdim = dim(var, ndims(var))
-    return if issorted(tdim)
-        searchsortedfirst(tdim, t0):searchsortedlast(tdim, t1)
-    else
-        findall(t -> t >= t0 && t <= t1, tdim)
-    end
-end
+# function find_indices(var, t0, t1)
+#     tdim = dim(var, ndims(var))
+#     return if issorted(tdim)
+#         searchsortedfirst(tdim, t0):searchsortedlast(tdim, t1)
+#     else
+#         findall(t -> t >= t0 && t <= t1, tdim)
+#     end
+# end
