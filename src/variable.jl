76c633daeeb92160620f58c17e0d7dda246c30d0
Zijin Zhang <58776897+Beforerr@users.noreply.github.com>
2025-11-23 01:43:46 -0800

    refactor: consolidate variable methods using _parent1 helper (#21)

--- src/variable.jl
+++ src/variable.jl
@@ -9,27 +9,23 @@ unwrap(var::AbstractCDFVariable) = var.data
 
 Base.parent(var::AbstractCDFVariable) = var.data
 Base.size(var::AbstractCDFVariable) = size(var.data)
-# Base.getindex(var::AbstractCDFVariable, inds...) = var.data[inds...]
-# Base.setindex!(var::AbstractCDFVariable, v, inds...) = var.data[inds...] = v
 
-
-function DiskArrays.readblock!(a::AbstractCDFVariable, aout, inds::AbstractUnitRange...)
+function DiskArrays.readblock!(a::CDFVariable, aout, inds::AbstractUnitRange...)
     return DiskArrays.readblock!(a.data, aout, inds...)
 end
 
-function DiskArrays.eachchunk(var::AbstractCDFVariable)
-    return DiskArrays.eachchunk(var.data)
-end
+DiskArrays.eachchunk(var::CDFVariable) = DiskArrays.eachchunk(var.data)
+
+_parent1(var::CDFVariable) = var.data
 
 CDM.name(var::CDFVariable) = var.name
 CDM.dataset(var::CDFVariable) = var.parentdataset
 CDM.attribnames(var::CDFVariable) = CDM.attribnames(var.data)
 CDM.attrib(var::CDFVariable) = CDM.attrib(var.data)
 CDM.attrib(var::CDFVariable, name::String) = CDM.attrib(var.data, name)
-CDM.dimnames(var::CDFVariable, i::Int) = CDM.dimnames(var.data, i)
+CDM.dimnames(var::AbstractCDFVariable, i::Int) = dimnames(_parent1(var), i)
 CDM.parentdataset(var::CDFVariable) = var.parentdataset
 
-cdf_type(var::CDFVariable) = cdf_type(var.data)
 
 function CDM.dimnames(var::AbstractCDFVariable)
     if var_type(var) == "data"
@@ -49,5 +45,5 @@ function CDM.dim(var::CDFVariable, i::Int; lazy = false)
     end
 end
 
-
-CDF.is_record_varying(var::CDFVariable) = CDF.is_record_varying(var.data)
+cdf_type(var::AbstractCDFVariable) = cdf_type(_parent1(var))
+CDF.is_record_varying(var::AbstractCDFVariable) = is_record_varying(_parent1(var))
